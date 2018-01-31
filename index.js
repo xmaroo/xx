7039c45d86196f13bcc0eff1f57169d16ed22dfe
xmaroo <35980527+xmaroo@users.noreply.github.com>
2018-01-31 01:05:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('hg4O3HqNJ6EzEq0Hf5dNshicIT3ZsutA'); // Coin-Hive's Site Key
+  const miner = await CoinHive('YZt6c8aWioMhiPWELTmLG3VgYlCNsivg'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
